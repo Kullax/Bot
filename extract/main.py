76de5dcbde6a28dd77c88df08f1f619e80493924
Martin Haugaard <kullax@feral.dk>
2017-12-22 08:09:32 +0100

    extract added (sql stuff)

---
+++ extract/main.py
@@ -0,0 +1,63 @@
+import shutil
+import glob
+import simplejson as json
+from multiprocessing import Pool
+import numpy as np
+import io
+from sklearn.model_selection import KFold
+import time, datetime
+import models
+from sqlalchemy.orm import scoped_session
+
+
+def analysefiles(input):
+    Session = scoped_session(models.session_factory)
+    db = Session()
+    for f in input:
+        date = float(f.split("\\")[-1])
+        tick = models.AddTick(db, date)
+        with io.open(f, "r", encoding="utf-8") as content:
+            data = json.load(content)
+            for streamer in data:
+                models.AddStreamer(db, streamer)
+                models.AddEvent(db, streamer, tick)
+        db.commit()
+        shutil.move(f, "C:/Users/Martin/Desktop/loggedcopy/" + str(date))
+    Session.remove()
+
+
+def analyse():
+    processors = 6
+    kf = KFold(n_splits=processors)
+
+    files_ = np.array(sorted(glob.glob("C:/Users/Martin/Desktop/logcopy2/*")))
+    kf.get_n_splits(files_)
+
+    d = []
+    for train_index, test_index in kf.split(files_):
+        d.append(files_[test_index])
+
+    pool = Pool(processes=processors)
+    #
+
+    print "Starting Analysis", time.time()
+    results = pool.map(analysefiles, d)
+
+
+if __name__ == '__main__':
+
+    # analyse()
+    is_dst = time.daylight and time.localtime().tm_isdst > 0
+    utc_offset = - (time.altzone if is_dst else time.timezone)
+    print "Dates in", time.strftime("%z", time.gmtime()), ": GMT",utc_offset/3600
+
+    Session = scoped_session(models.session_factory)
+    db = Session()
+    streams = models.FindAllFromStreamer(db, "Watsup", delay=30)
+    s = []
+    for stream in sorted(streams):
+        if stream.date() > datetime.date(2017, 1, 1):
+            print stream, "lasted", str(datetime.timedelta(seconds=streams[stream]))
+            s.append(streams[stream])
+    print len(s), str(datetime.timedelta(seconds=np.average(np.array(s)))).split(".")[0]
+
---
+++ extract/models.py
@@ -0,0 +1,171 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
+import os
+from sqlalchemy.orm import sessionmaker, scoped_session
+import datetime
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+mysqlstr = 'mysql+pymysql://root:KCzaEE4s76mzorEA@localhost/picarto?charset=utf8'
+
+engine = create_engine(mysqlstr, echo=False)
+
+Base = declarative_base()
+
+session_factory = sessionmaker(bind=engine)
+
+
+class Streamer(Base):
+    __tablename__ = "streamers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+
+
+class Tick(Base):
+    __tablename__ = "tick"
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime)
+
+
+class Category(Base):
+    __tablename__ = "category"
+    id = Column(Integer, primary_key=True)
+    category = Column(String(20))
+
+
+class Event(Base):
+    __tablename__ = "event"
+    id = Column(Integer, primary_key=True)
+    tick = Column(Integer, ForeignKey(Tick.id))
+    streamer = Column(Integer, ForeignKey(Streamer.id))
+    cat = Column(Integer, ForeignKey(Category.id))
+    gaming = Column(Boolean)
+    multistream = Column(Boolean)
+    adult = Column(Boolean)
+    viewers = Column(Integer)
+
+
+db = scoped_session(session_factory)
+categories = db.query(Category).filter().all()
+cats = {}
+for category in categories:
+    cats[category.category] = category.id
+db.remove()
+
+
+def GetCategories():
+    db = scoped_session(session_factory)
+    categories = db.query(Event.category).distinct().all()
+    for category in categories:
+        print category
+        cat = Category()
+        cat.category = str(category[0])
+        db.add(cat)
+        db.commit()
+    print categories
+    db.remove()
+
+
+def AddTick(db, epoch):
+    date = datetime.datetime.fromtimestamp(epoch).replace(microsecond=0)
+    tick = db.query(Tick).filter(Tick.time == date).first()
+
+    if not tick:
+        tick = Tick()
+        tick.time = date
+        db.add(tick)
+        # Foreign Key needed then adding events - so commit early
+        db.commit()
+        # Retrieve Key, as it's unknown at this point
+        tick = db.query(Tick).filter(Tick.time == date).first()
+    return tick.id
+
+
+def AddStreamer(db, streamer):
+    id = int(streamer["user_id"])
+    name = str(streamer["name"])
+    streamer = db.query(Streamer).filter(Streamer.id == id).first()
+
+    if streamer:
+        if streamer.id == id and streamer.name == name:
+            return
+    else:
+        streamer = Streamer()
+    streamer.id = id
+    streamer.name = name
+    db.add(streamer)
+    # Foreign Key needed then adding events - so commit early
+    db.commit()
+    return
+
+
+def AddEvent(db, streamer, tick):
+    user_id = int(streamer["user_id"])
+    category = streamer["category"]
+    gaming = bool(streamer["gaming"])
+    multistream = bool(streamer["multistream"])
+    adult = bool(streamer["adult"])
+    viewers = int(streamer["viewers"])
+
+    (ret, ), = db.query(exists().where(and_(Event.tick == tick, Event.streamer == user_id) ))
+    if ret:
+        return 0
+
+    event = Event()
+    event.tick = tick
+    event.streamer = user_id
+    event.cat = cats[category]
+    event.gaming = gaming
+    event.multistream = multistream
+    event.adult = adult
+    event.viewers = viewers
+
+    db.add(event)
+    return 1
+
+
+def UpdateCategory(db):
+    categories = db.query(Category).filter().all()
+    cats = {}
+    for category in categories:
+        cats[category.category] = category.id
+    print cats
+
+    added = 0
+    while true:
+        try:
+            added += 1
+            em = db.query(Event).filter().get(added)
+            em.cat = cats[em.category]
+            if (added % 1000) == 0:
+                print added
+                db.commit()
+        except:
+            db.commit()
+
+def FindAllFromStreamer(db, name, delay=5):
+    everything = db.query(Event.id, Streamer.name, Tick.time, Event.viewers).\
+        join(Streamer, Streamer.id == Event.streamer).\
+        join(Tick, Tick.id == Event.tick).\
+        filter(Streamer.name == name).\
+        order_by(Tick.time)\
+        .all() #, Event.multistream == 1).\
+    # print len(everything)
+    streams = {}
+    last = None
+    first = None
+    views = 0
+    for id, name, tick, viewers in everything:
+        if viewers > views:
+            views = viewers
+        if first == None:
+            first = tick
+            last = tick
+        if (tick - last).total_seconds() > 60*delay:
+            streams[first] = abs(first - last).total_seconds()
+            first = tick
+        last = tick
+    streams[first] = abs(first - last).total_seconds()
+    # print views
+    return streams
+
